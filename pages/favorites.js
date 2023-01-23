da939aff331f8de4cdfc3755c5640a666f5c7211
PinkDwarf <margareta.ifrim@outlook.com>
2023-01-23 19:14:01 +0000

    First commit

---
+++ pages/favorites.js
@@ -0,0 +1,30 @@
+import view from "../utils/view.js";
+import checkFavorite from "../utils/checkFavorite.js";
+import store from "../store.js";
+import Story from "../components/Story.js";
+
+
+export default function Favorites() {
+  const { favorites } = store.getState();
+  const hasFavorites = favorites.length > 0;
+
+  view.innerHTML = `<div>
+    ${hasFavorites ? favorites.map(story => Story({
+      ...story, 
+      isFavorite: checkFavorite(favorites, story)
+    })).join('') : "Add some favorites"}
+  </div>`;
+
+  document.querySelectorAll(".favorite").forEach(favoriteButton => {
+    favoriteButton.addEventListener("click", function() {
+      const story = JSON.parse(this.dataset.story);
+      const isFavorited = checkFavorite(favorites, story);
+      if(isFavorited) {
+        store.dispatch({ type: "REMOVE_FAVORITE", payload: { favorite: story } })
+      }else {
+        store.dispatch({ type: "ADD_FAVORITE", payload: { favorite: story } })
+      }
+      Favorites();
+    });
+  });
+}
---
+++ utils/baseUrl.js
@@ -0,0 +1,2 @@
+
+export default 'https://node-hnapi.herokuapp.com';
